1cadcacf2141070c1cd0669f78741a0d212f5436
edy477 <edy47@mesoamericana.edu.gt>
2020-10-31 18:16:09 -0600

    last commit from this week

--- components/CardProject2.js
+++ components/CardProject2.js
@@ -4,7 +4,7 @@ const CardProject2 = () => {
 // <img  src="/images/image1.jpg " ></img>
     return (
         <a href="https://bitcoin-dashboard-git-master.edy47.vercel.app/" target="_blank" rel="noopener noreferrer" className=" pts w-1/4  h-1/4 shadow-2xl">
-        <div className="cardproject ">
+        <div className="cardproject h-full ">
 
             <img  src="/images/pic_2.png " ></img>
             <h1 className="text-center mt-3 font-semibold">Bitcoin Dashboard</h1>
